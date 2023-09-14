2922482d282ce8ca0e1196e8e600d1628e2af123
jim125 <pingandu590@gmail.com>
2023-09-14 18:33:21 +0800

    delete UpYunConfig.java delete UpYunService.java modify pom.xml modify application.properties

--- src/main/java/com/maindark/livestream/controller/UploadFileController.java
+++ src/main/java/com/maindark/livestream/controller/UploadFileController.java
@@ -1,7 +1,6 @@
 package com.maindark.livestream.controller;
 
 import com.maindark.livestream.result.Result;
-import com.maindark.livestream.upyun.UpYunService;
 import com.maindark.livestream.util.UUIDUtil;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,17 +13,15 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/api/v1/uploadImg")
 public class UploadFileController {
 
-    @Resource
-    UpYunService upYunService;
+
+
 
     @GetMapping("/add")
     public Result<String>  upLoadFile(@RequestParam("file")MultipartFile file) throws Exception{
         String fileName = file.getOriginalFilename();
         String suffixName = fileName.substring(fileName.lastIndexOf("."));
         fileName = UUIDUtil.uuid() + suffixName;
-        // upload upYun
         byte[] bytes = file.getBytes();
-        upYunService.uploadImg(bytes,fileName);
         return Result.success(fileName);
     }
 
