eebb8ecc7976251330b4f533a3abcd39946a4bf8
Jim124 <pingandu590@gmail.com>
2023-09-26 19:36:14 +0800

    Changes

--- src/main/java/com/maindark/livestream/aws/AWSConfig.java
+++ src/main/java/com/maindark/livestream/aws/AWSConfig.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Component;
 @Data
 @Getter
 public class AWSConfig {
+
     private String accessKey;
     private String secretKey;
     private String region;
