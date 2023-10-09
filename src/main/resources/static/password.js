7671d30b58b3eb03d8deaf603a185bf1c0d8e9c7
Jim124 <pingandu590@gmail.com>
2023-10-09 15:30:31 +0800

    add nginx configuration

--- src/main/resources/static/password.js
+++ src/main/resources/static/password.js
@@ -1,5 +1,5 @@
 <script type="text/javascript" th:src="@{/js/md5.min.js}"></script>
-var g_passsword_salt="1a2b3c4d"
-var salt = g_passsword_salt;
-var str = ""+salt.charAt(0)+salt.charAt(2) + inputPass +salt.charAt(5) + salt.charAt(4);
-var password = md5(str);
+const g_passsword_salt = "1a2b3c4d";
+const salt = g_passsword_salt;
+const str = "" + salt.charAt(0) + salt.charAt(2) + inputPass + salt.charAt(5) + salt.charAt(4);
+const password = md5(str);
