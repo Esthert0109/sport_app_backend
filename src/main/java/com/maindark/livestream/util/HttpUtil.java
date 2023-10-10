df3dc2c41cb98ff791a9fd2c7cb7d479df657879
Jim124 <pingandu590@gmail.com>
2023-10-10 13:58:39 +0800

    refactor HttpUtil

--- src/main/java/com/maindark/livestream/util/HttpUtil.java
+++ src/main/java/com/maindark/livestream/util/HttpUtil.java
@@ -1,21 +1,22 @@
 package com.maindark.livestream.util;
 
-import com.alibaba.druid.util.StringUtils;
-import com.alibaba.fastjson2.JSON;
-import com.alibaba.fastjson2.JSONObject;
-import com.maindark.livestream.exception.GlobalException;
-import com.maindark.livestream.result.CodeMsg;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Slf4j
 public class HttpUtil {
 
     public static String getNaMiData(String url){
-        RestTemplate restTemplate = new RestTemplate();
         log.info("send nami url:{}",url);
-        String result = restTemplate.getForObject(url,String.class);
+        WebClient webClient = WebClient.create();
+        WebClient.ResponseSpec responseSpec = webClient.get()
+                .uri(url)
+                .retrieve();
+        String result = responseSpec.bodyToMono(String.class).block();
+       // RestTemplate restTemplate = new RestTemplate();
+       // String result = restTemplate.getForObject(url,String.class);
         return result;
-
     }
 }
