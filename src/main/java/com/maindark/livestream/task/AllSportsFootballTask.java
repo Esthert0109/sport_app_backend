cc3a92e6ee4619b637f7a25b470b534d4d7707e1
Jim124 <pingandu590@gmail.com>
2023-10-25 19:37:47 +0800

    modify AllSportsFootballTask

--- src/main/java/com/maindark/livestream/task/AllSportsFootballTask.java
+++ src/main/java/com/maindark/livestream/task/AllSportsFootballTask.java
@@ -40,7 +40,7 @@ public class AllSportsFootballTask {
    AllSportsHomeMatchLineUpDao allSportsHomeMatchLineUpDao;
 
 
-    //@Scheduled(cron = "0 */2 * * * ? ")
+    @Scheduled(cron = "0 */2 * * * ? ")
     public void getAllSportsFootballMatchLineUp(){
         String url = allSportsConfig.getAllSportsApi(allSportsConfig.getLivescore());
         String result = HttpUtil.getAllSportsData(url);
