408e3173327c2d32829ef8dce85120e0791f36bc
Jim124 <pingandu590@gmail.com>
2024-03-11 09:34:05 +0800

    modify PopularService

--- src/main/java/com/maindark/livestream/service/PopularGamesService.java
+++ src/main/java/com/maindark/livestream/service/PopularGamesService.java
@@ -3,7 +3,6 @@ package com.maindark.livestream.service;
 import com.maindark.livestream.dao.PopularGamesDao;
 import com.maindark.livestream.vo.PopularGamesVo;
 import jakarta.annotation.Resource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
