49b40a456b65fe3c9b06bb28d00c619be53e621e
Jim124 <pingandu590@gmail.com>
2024-03-12 21:01:36 +0800

    modify FeiHingApiFootballController

--- src/main/java/com/maindark/livestream/feiJing/FeiHingApiFootballController.java
+++ src/main/java/com/maindark/livestream/feiJing/FeiHingApiFootballController.java
@@ -1,7 +1,6 @@
 package com.maindark.livestream.feiJing;
 
 import com.maindark.livestream.result.Result;
-import com.maindark.livestream.vo.FootballMatchVo;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
