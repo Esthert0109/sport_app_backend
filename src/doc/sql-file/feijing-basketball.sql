71bc900276274b03147cded9132c12911bf8f3e3
TTW0407 <ttwei0407@gmail.com>
2024-03-15 13:41:34 +0800

    Move feijing-basketball.sql file

--- src/doc/sql-file/feijing-basketball.sql
+++ src/doc/sql-file/feijing-basketball.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS `feijing_basketball_match`;
-CREATE TABLE `feijing_basketball_match`
+DROP TABLE IF EXISTS `fei_jing_basketball_match`;
+CREATE TABLE `fei_jing_basketball_match`
 (
     `match_id`         bigint(20) NOT NULL COMMENT '赛程ID',
     `competition_id`   bigint(20)    NOT NULL COMMENT '联赛ID',
@@ -8,11 +8,11 @@ CREATE TABLE `feijing_basketball_match`
     `match_time`       varchar(255) COMMENT '比赛时间 GMT+8 格式:yyyy/MM/dd',
     `match_state`      int(2)     NOT NULL COMMENT '比赛状态 0：未开赛、1：一节、2：二节、3：三节、4：四节、5：1\'OT、6：2\'OT、7：3\'OT、50：中场、-1：完场、-2：待定、-3：中断、-4：取消、-5：推迟',
     `home_team_id`     bigint(20) NOT NULL COMMENT '主队ID',
-    `home_team_eng`    varchar(255) COMMENT '主队名-英文',
-    `home_team_cn`     varchar(255) COMMENT '主队名-简体',
+    `home_team_en`    varchar(255) COMMENT '主队名-英文',
+    `home_team_chs`     varchar(255) COMMENT '主队名-简体',
     `away_team_id`     bigint(20) NOT NULL COMMENT '客队ID',
-    `away_team_eng`    varchar(255) COMMENT '客队名-英文',
-    `away_team_ch`     varchar(255) COMMENT '客队名-简体',
+    `away_team_en`    varchar(255) COMMENT '客队名-英文',
+    `away_team_chs`     varchar(255) COMMENT '客队名-简体',
     `home_score`       int(3)     NOT NULL COMMENT '主队总分',
     `away_score`       int(3)     NOT NULL COMMENT '客队总分',
     `season`           varchar(255) COMMENT '赛季',
