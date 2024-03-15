a5cc6fda39bc5d9918b406911a1917b745c3d96c
TTW0407 <ttwei0407@gmail.com>
2024-03-15 13:42:59 +0800

    Update FeiJingApiBasketballController

---
+++ src/main/java/com/maindark/livestream/feiJing/FeiJingApiBasketballController.java
@@ -0,0 +1,36 @@
+package com.maindark.livestream.feiJing;
+
+import com.maindark.livestream.result.Result;
+import jakarta.annotation.Resource;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/FeiJing/basketball")
+public class FeiJingApiBasketballController {
+    @Resource
+    FeiJingApiBasketballService feijingApiBasketballService;
+
+//    @GetMapping("/teams")
+//    public Result<Boolean> getAllTeams(){
+//        feijingApiBasketballService.getAllTeams();
+//        return Result.success(true);
+//    }
+
+//    @GetMapping("/matches/{matchDate}")
+//    public Result<List<Map<String,Object>>> getBasketball(@PathVariable("matchDate")String matchDate){
+//        List<Map<String,Object>> res = feijingApiBasketballService.getMatchByDate(matchDate);
+//        return Result.success(res);
+//    }
+    @GetMapping("/pending")
+    @Select("select * from fei_jing_basketball_match where match_state = 0")
+    public Result<Boolean> getUpcomingMatches(){
+        return Result.success(true);
+    }
+}
