cacb949943d0ea8ed8fa47bfd8e8b2926f8bb881
TTW0407 <ttwei0407@gmail.com>
2024-03-19 11:13:10 +0800

    Update FeiJingBasketballLatestMatchesTask

--- src/main/java/com/maindark/livestream/task/FeiJingBasketballLatestMatchesTask.java
+++ src/main/java/com/maindark/livestream/task/FeiJingBasketballLatestMatchesTask.java
@@ -24,33 +24,59 @@ public class FeiJingBasketballLatestMatchesTask {
     FeiJingBasketballConfig feiJingBasketballConfig;
 
 
-    @Scheduled(cron = "0 0 * * * *")
-    public void updateMatches() {
+    @Scheduled(cron = "* * */1 * * *")
+    public void getAllMatches() {
+        String url = feiJingBasketballConfig.getMatch();
+        String result = HttpUtil.sendGet(url);
+        Map<String, Object> resultObj = JSON.parseObject(result, Map.class);
+        List<Map<String, Object>> matchList = (List<Map<String, Object>>) resultObj.get("matchList");
+        if (matchList != null && !matchList.isEmpty()) {
+            matchList.forEach(match -> {
+
+                Integer matchId = (Integer) match.get("matchId");
+                Integer competitionId = (Integer) match.get("leagueId");
+                String leagueEn = (String) match.get("leagueEn");
+                String leagueChs = (String) match.get("leagueChs");
+                String matchTime = (String) match.get("matchTime");
+                Integer matchState = (Integer) match.get("matchState");
+                Integer homeTeamId = (Integer) match.get("homeTeamId");
+                String homeTeamEn = (String) match.get("homeTeamEn");
+                String homeTeamChs = (String) match.get("homeTeamChs");
+                Integer awayTeamId = (Integer) match.get("awayTeamId");
+                String awayTeamEn = (String) match.get("awayTeamEn");
+                String awayTeamChs = (String) match.get("awayTeamChs");
+                Integer homeScore = (Integer) match.get("homeScore");
+                Integer awayScore = (Integer) match.get("awayScore");
+                String season = (String) match.get("season");
+                String kind = (String) match.get("matchKind");
+                String updatedDate = (String) match.get("updateTime");
+
+                FeijingBasketballMatch feijingBasketballMatch = new FeijingBasketballMatch();
+
+                feijingBasketballMatch.setMatchId(matchId);
+                feijingBasketballMatch.setCompetitionId(competitionId);
+                feijingBasketballMatch.setLeagueEn(leagueEn);
+                feijingBasketballMatch.setLeagueChs(leagueChs);
+                feijingBasketballMatch.setMatchTime(matchTime);
+                feijingBasketballMatch.setMatchState(matchState);
+                feijingBasketballMatch.setHomeTeamId(homeTeamId);
+                feijingBasketballMatch.setHomeTeamEn(homeTeamEn);
+                feijingBasketballMatch.setHomeTeamChs(homeTeamChs);
+                feijingBasketballMatch.setAwayTeamId(awayTeamId);
+                feijingBasketballMatch.setAwayTeamEn(awayTeamEn);
+                feijingBasketballMatch.setAwayTeamChs(awayTeamChs);
+                feijingBasketballMatch.setHomeScore(homeScore);
+                feijingBasketballMatch.setAwayScore(awayScore);
+                feijingBasketballMatch.setSeason(season);
+                feijingBasketballMatch.setKind(kind);
+                feijingBasketballMatch.setUpdatedDate(updatedDate);
 
 
-        try {
-            String url = feiJingBasketballConfig.getMatch();
-            String result = HttpUtil.sendGet(url);
-            Map<String, Object> resultObj = JSON.parseObject(result, Map.class);
-            List<Map<String, Object>> matchList = (List<Map<String, Object>>) resultObj.get("matchList");
-
-            if (matchList != null && !matchList.isEmpty()) {
-                for (Map<String, Object> match : matchList) {
-
-                    FeijingBasketballMatch feijingBasketballMatch = parseMatch(match);
+                int existed = feijingBasketballMatchDao.queryExisted(matchId);
+                if (existed <= 0) {
                     feijingBasketballMatchDao.insertData(feijingBasketballMatch);
                 }
-            }
-
-            log.info("Basketball match update completed.");
-        } catch (Exception e) {
-            log.error("Failed to update basketball matches: {}", e.getMessage());
+            });
         }
-    }
-
-
-    private FeijingBasketballMatch parseMatch(Map<String, Object> match) {
-
-        return null;
     }
 }
